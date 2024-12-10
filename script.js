3808aeedc2f9073afc05d9948fc8e65ffef45db7
jwim5819 <jwim5819@gmail.com>
2024-12-11 08:56:42 +0900

    24.12.11/1

--- script.js
+++ script.js
@@ -2,36 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const quoteElement = document.getElementById("quote_1");
   const authorElement = document.getElementById("quote_2");
   const imageElement = document.getElementById("image");
-  const refreshButton = document.getElementById("refreshButton");
+  const bgElement = document.getElementById("bg"); // 전체 배경 요소 선택
 
-  // 배경 이미지 목록
-  // const backgroundImages = [
-  //   "https://i.ibb.co/n668rcp/Kakao-Talk-20240616-213639398.jpg",
-  //   "https://i.ibb.co/RHkQmC1/Kakao-Talk-20240616-213639398-03.jpg",
-  //   "https://i.ibb.co/85TN3Cp/Kakao-Talk-20240616-213639398-04.jpg",
-  //   "https://i.ibb.co/GQtpZLd/Kakao-Talk-20240616-213639398-05.jpg",
-  //   "https://i.ibb.co/ChXB7BF/Kakao-Talk-20240616-213639398-06.jpg",
-  //   "https://i.ibb.co/pwgWzPF/Kakao-Talk-20240616-213639398-01.jpg",
-  //   "https://i.ibb.co/3MWYxqr/Kakao-Talk-20240616-213639398-02.jpg",
-  // ];
+  let backgroundImages = [];
+  const totalImages = 30; // 이미지 총 개수
 
-  const backgroundImages = [
-      "assets/images/1.png",
-      "assets/images/2.png",
-      "assets/images/3.png",
-      "assets/images/4.png",
-      "assets/images/5.png",
-      "assets/images/6.png",
-      "assets/images/7.png",
-      "assets/images/8.png",
-      "assets/images/9.png",
-      "assets/images/10.png",
-      "assets/images/11.png",
-      "assets/images/12.png",
-      "assets/images/13.png",
-      "assets/images/14.png",
-      "assets/images/15.png",
-  ];
+  for (let i = 1; i <= totalImages; i++) {
+    backgroundImages.push(`assets/background/bg (${i}).png`);
+  }
 
   // CSV 파일 경로
   const csvFilePath = "assets/data/elan_talk_data.csv";
@@ -58,8 +36,8 @@ document.addEventListener("DOMContentLoaded", () => {
       });
   }
 
-  // 새로고침 버튼 이벤트
-  refreshButton.addEventListener("click", updateContent);
+  // 배경 요소 클릭 시 새로고침
+  bgElement.addEventListener("click", updateContent);
 
   // 초기 콘텐츠 로드
   updateContent();
